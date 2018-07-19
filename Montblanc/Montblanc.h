32c90413da864e84e35ab83756a4b8cecb768686
natmark <natmark0918@gmail.com>
2018-07-19 16:33:20 +0900

    :new: Add project

---
+++ Montblanc/Montblanc.h
@@ -0,0 +1,19 @@
+//
+//  Montblanc.h
+//  Montblanc
+//
+//  Created by AtsuyaSato on 2018/07/19.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Montblanc.
+FOUNDATION_EXPORT double MontblancVersionNumber;
+
+//! Project version string for Montblanc.
+FOUNDATION_EXPORT const unsigned char MontblancVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Montblanc/PublicHeader.h>
+
+
