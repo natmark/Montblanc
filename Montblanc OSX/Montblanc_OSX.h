60a31d7db23faac126b8a10337270223ef093273
natmark <natmark0918@gmail.com>
2018-07-19 16:38:50 +0900

    :new: Add OS X Target

---
+++ Montblanc OSX/Montblanc_OSX.h
@@ -0,0 +1,19 @@
+//
+//  Montblanc_OSX.h
+//  Montblanc OSX
+//
+//  Created by AtsuyaSato on 2018/07/19.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Montblanc_OSX.
+FOUNDATION_EXPORT double Montblanc_OSXVersionNumber;
+
+//! Project version string for Montblanc_OSX.
+FOUNDATION_EXPORT const unsigned char Montblanc_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Montblanc_OSX/PublicHeader.h>
+
+
