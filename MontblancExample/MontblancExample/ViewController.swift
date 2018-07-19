b4e078f7df5d604cc89eac2a673c6c660f062227
natmark <natmark0918@gmail.com>
2018-07-19 19:10:44 +0900

    :new: Add MontblancExample

---
+++ MontblancExample/MontblancExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MontblancExample
+//
+//  Created by AtsuyaSato on 2018/07/19.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+import UIKit
+import Montblanc
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
